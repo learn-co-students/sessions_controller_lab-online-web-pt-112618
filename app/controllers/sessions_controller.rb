2dcdf52a7554dd320dfc6861b5e922b1a0365345
Michael Schiumo <mschiumo18@gmail.com>
2019-05-17 12:02:17 -0400

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,6 @@ class SessionsController < ApplicationController
 
   def create
     if params[:name].nil?
-      
       redirect_to controller: 'sessions', action: 'new'
     else session[:name] = params[:name]
       redirect_to '/'
